7837a5231e27e6df679b85366d73031dbe08ad58
Arthur-olive <joao37725@gmail.com>
2024-06-22 14:06:44 -0300

    cards

---
+++ CeluStore.js
@@ -0,0 +1,53 @@
+function generatePrice(){
+    randomNumber = Math.random() * 100
+    return (Math.random() * 100).toFixed(2)
+}
+
+function generateName(){
+    const names = ["Iphme","Samsunga","Moto Rola","Xiaomi","Opporra","Vivo"]
+    randomNames = Math.floor(Math.random() * (names.length))
+    return names[randomNames]
+}
+
+function generateDescription(){
+    const desc = ["Branco","Azul Meia noite","Gold","Rose","Verde","Coral","(Red)"]
+    randomDesc = Math.floor(Math.random() * (desc.length))
+    return desc[randomDesc]
+}
+
+async function generateImage(){
+    return fetch ("https://api.thecatapi.com/v1/images/search").then(
+        response => response.json()
+    ).then(
+        data => data[0].url
+    )
+}
+
+async function placeCellCard(){
+
+    const cardGrid = document.getElementById("cards-grid")
+
+    const cellName = generateName()
+    const cellDesc = generateDescription()
+    const cellPrice = generatePrice()
+    const image = await generateImage()
+
+    const card = `
+    <div class="card">
+        <img alt="Cell" src="${image}">
+        <h4>Marca: ${cellName}</h4>
+        <p class="description">${cellDesc}</p>
+        <div>
+            <p class="price">Valor: $${cellPrice}</p>
+        </div>
+    </div>
+    `
+
+    cardGrid.innerHTML += card
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+   for (let i = 0; i < 45; i++){
+    placeCellCard()
+   }
+});
